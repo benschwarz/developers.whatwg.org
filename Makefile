b58458511df8cde18bb56c11ec4f730b5a3be7f7
Lincoln Stoll <l@lds.li>
2011-02-21 15:34:12 +1100

    Performance enhancement for build tasks
    Break up the Makefile a little, to avoid repeating
    tasks that work on infrequent changing data. Now,
    in general usage a 'make' by itself is sufficent
    to re-generate output. A make clean is only 
    required if upstream data changes.

--- Makefile
+++ Makefile
@@ -7,14 +7,13 @@ PATCH=patch
 SPLITTER=html5-tools/spec-splitter/spec-splitter.py
 SPLITTERFLAGS=--html5lib-serialiser
 
-postprocess: LOG
-	rake postprocess:execute
+postprocess: clean-output process_assets LOG
+	rake --trace postprocess:execute
 
 LOG: index.html $(SPLITTER)
 	$(PYTHON) $(SPLITTER) $(SPLITTERFLAGS) $< ./public > LOG
 
 index.html: html5-full.html anolis/anolis
-	$(RUBY) tidy.rb $<
 	$(PYTHON) anolis/anolis \
 	  --parser=lxml.html \
 	  --filter=.impl \
@@ -23,11 +22,19 @@ index.html: html5-full.html anolis/anolis
 
 html5-full.html:
 	$(CURL) --compressed http://www.whatwg.org/specs/web-apps/current-work/dev-index > $@
+	$(RUBY) tidy.rb $@
 
-clean:
-	$(RM) -r public/**/*.html
-	$(RM) LOG
+process_assets:
+	$(RUBY) assets.rb
+
+clean: clean-output
 	$(RM) html5-full.html
+
+clean-output:
+	$(RM) LOG
+	$(RM) -r public/**/*.html
+	$(RM) -r public/css/*.css
+	$(RM) -r public/*.manifest
 
 anolis/anolis:
 	$(HG) clone http://hg.hoppipolla.co.uk/anolis/
--- tidy.rb
+++ tidy.rb
@@ -1,38 +1,14 @@
 # encoding: UTF-8
 require "rubygems"
-require "bundler/setup"
+require "bundler"
 
-require "nokogiri"
-require "sass"
-require "yui/compressor"
-require "fileutils"
-require "peach"
+Bundler.require
 
 doc = Nokogiri::HTML(File.open(ARGV[0], "r"))
-js_compressor = YUI::JavaScriptCompressor.new
 
 # Remove all scripts and styles included
 doc.css("link[rel='stylesheet'], style, link[href^='data:text/css']").remove
 doc.css("script").remove
-
-# Compile master.scss
-# Write output to public/css
-Dir.chdir("sass") do
-  %w(all desktop handheld).peach do |device|
-    css = Sass.compile_file("#{device}.scss", :style => :compressed)
-    File.open("../public/css/#{device}.css", "w"){|buffer| buffer << css }
-  end
-end
-
-# Compress * javascripts to application.js
-Dir.chdir("javascript") do
-  application = "../public/javascript/application.js"
-  
-  Dir["**/*.js"].peach do |javascript_filepath|
-    compressed = js_compressor.compress(File.open(javascript_filepath, "r")) + "\n"
-    File.open(application, "a"){|buffer| buffer << compressed }
-  end
-end
 
 # Wrap the header group in an a referencing "/"
 doc.at("header.head hgroup h1").replace("<h1><a href='/' rel='home'>HTML5</a></h1>")
